01b390384d6d96f01733e5cd563acebbb74703a2
erbium6 <86596363+erbium6@users.noreply.github.com>
2021-12-12 14:00:39 +0200

    edit task 9

--- js/task-09.js
+++ js/task-09.js
@@ -2,16 +2,13 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-const changeColorBtn = document.querySelector('.change-color');
-const backgroundColor = document.querySelector('.color')
+const refs = {
+   changeColorBtn: document.querySelector('.change-color'),
+  backgroundColorSpan: document.querySelector('.color'),
+   
+};
 
-const changeColor = () => {
+refs.changeColorBtn.addEventListener('click', function () {
   document.body.style.backgroundColor = getRandomHexColor();
-};
-
-const textColor = () => {
-  backgroundColor.textContent = getRandomHexColor();
-};
-
-changeColorBtn.addEventListener('click', changeColor);
-changeColorBtn.addEventListener('click', textColor);
+  refs.backgroundColorSpan.textContent = document.body.style.backgroundColor;
+})
